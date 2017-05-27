8baa1610a15346eb61f22753a7adf57b038bd0e1
Gabriel Marshall Rebello <GabrielMR-Dragon@hotmail.com>
2017-05-27 20:45:21 -0300

    Inicio do protótipo de movimentação
    Adicionada uma nova alteração no .gitignore para evitar arquivos EXE;
    Adicionado os códigos do professor de movimentação, visão e colisão com
    leves alterações da nossa parte;
    Adicionada a ideia inicial de hierarquia de pastas e classes do projeto;
    Adicionado um código em C da Tabela ASCII para consulta de caracteres.

---
+++ PrototipoMecanica1/PrototipoMecanica1/Entity/Body/Character/character.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Audio;
+
+namespace PrototipoMecanica1
+{
+  public class Character : Body
+  {
+    public float health = 100f;
+
+    public float fireRate = 10f; //hz
+
+    float fireTimer = 0;
+
+    Vector2 shootDir = new Vector2(1, 0);
+
+    public Character(Vector2 initPos) : base(initPos) { }
+
+    public virtual bool WantsToFire() { return false; }
+
+    public override void Update(GameTime gameTime)
+    {
+      float dt = (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+      base.Update(gameTime);
+
+      Vector2 dir = GetDir();
+      if (dir.Length() > 0)
+        shootDir = dir;
+
+      if (fireTimer <= 0)
+      {
+        fireTimer = 1.0f / fireRate;
+
+        if (WantsToFire())
+        {
+          World.entities.Add(new Fire(this, pos, shootDir));
+        }
+      }
+      else
+      {
+        fireTimer -= dt;
+      }
+	  
+	  if (health <= 0.0f)
+		World.entities.Remove(this);
+    }
+  }
+}

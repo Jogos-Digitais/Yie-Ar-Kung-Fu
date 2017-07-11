3c2a6ee0d25cc04ebc0d6c4f4a54e1d8cb6b5188
Gabriel Marshall Rebello <GabrielMR-Dragon@hotmail.com>
2017-07-11 09:56:44 -0300

    Atualização [2] - 11/07/2017
    Limpeza de código:
    
    + Deletado o arquivo Button.cs, jamais utilizado;
    + Ajustado o nome do protótipo de 3 para 4.

--- PrototipoMecanica4/PrototipoMecanica4/Properties/AssemblyInfo.cs
+++ PrototipoMecanica4/PrototipoMecanica4/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PrototipoMecanica3")]
-[assembly: AssemblyProduct("PrototipoMecanica3")]
+[assembly: AssemblyTitle("PrototipoMecanica4")]
+[assembly: AssemblyProduct("PrototipoMecanica4")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyCompany("")]
